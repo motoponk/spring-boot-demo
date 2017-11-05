9ef5eac0b2b196ced74d32e2f090eb9c951446b5
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2017-11-05 11:55:54 +0530

    Fixing duplicate result issue.

--- src/main/java/com/sivalabs/moviefinder/repository/MovieRepository.java
+++ src/main/java/com/sivalabs/moviefinder/repository/MovieRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
  * Repository to perform database operations on Movie entity
  */
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-    @Query("select m from Movie m join fetch m.genres where lower(m.title) like lower(concat('%', ?1,'%'))")
+    @Query("select distinct m from Movie m join fetch m.genres where lower(m.title) like lower(concat('%', ?1,'%'))")
     List<Movie> findMovies(String searchKey);
 
     @Query("select m from Movie m where ?1 MEMBER OF m.genres")
